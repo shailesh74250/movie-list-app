1dd34d523f938abae6a1f923f0435e620b9ad794
shailesh74250 <smourya7425@gmail.com>
2020-08-29 18:20:52 +0530

    setup react project with webpack and babel

---
+++ src/containers/List.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import Card from '../components/Card';
+class List extends Component {
+  render() {
+    return <h1>movieList</h1>;
+  }
+}
+export default List;
